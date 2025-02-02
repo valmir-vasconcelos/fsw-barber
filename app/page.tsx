85fabf4ccbcab5a2e34e11be23b0589c1255431d
Valmir Vasconcelos <valmir.bsi@gmail.com>
2025-02-02 00:39:03 -0300

    feat: add BookingItem component

--- app/page.tsx
+++ app/page.tsx
@@ -4,13 +4,10 @@ import { Button } from "./_components/ui/button";
 import { Input } from "./_components/ui/input";
 import Image from "next/image";
 import { Card, CardContent } from "./_components/ui/card";
-import { Badge } from "./_components/ui/badge";
-import { Avatar, AvatarImage } from "./_components/ui/avatar";
 import { db } from "./_lib/prisma";
 import BarbershopItem from "./_components/barbershop-item";
-
-
-
+import { quickSearchOptions } from "./_constants/search";
+import BookingItem from "./_components/booking-item";
 
 export default async function Home() {
 
@@ -35,60 +32,21 @@ export default async function Home() {
         </div>
 
         <div className="mt-6 flex gap-3 overflow-x-scroll [&::-webkit-scrollbar]:hidden">
-          <Button className="gap-2" variant="secondary">
-            <Image src="/cabelo.svg" width={16} height={16} alt="cabelo" />
-            Cabelo
-          </Button>
-          <Button className="gap-2" variant="secondary">
-            <Image src="/barba.svg" width={16} height={16} alt="barba" />
-            Barba
-          </Button>
-          <Button className="gap-2" variant="secondary">
-            <Image src="/acabamento.svg" width={16} height={16} alt="acabamento" />
-            Acabamento
-          </Button>
-          <Button className="gap-2" variant="secondary">
-            <Image src="/cabelo.svg" width={16} height={16} alt="cabelo" />
-            Cabelo
-          </Button>
-          <Button className="gap-2" variant="secondary">
-            <Image src="/barba.svg" width={16} height={16} alt="barba" />
-            Barba
-          </Button>
-          <Button className="gap-2" variant="secondary">
-            <Image src="/acabamento.svg" width={16} height={16} alt="acabamento" />
-            Acabamento
-          </Button>
+          {
+            quickSearchOptions.map((option) => (
+              <Button className="gap-2" variant="secondary" key={option.title}>
+                <Image src={option.imageUrl} width={16} height={16} alt={option.title} />
+                {option.title}
+              </Button>
+            ))
+          }
         </div>
 
         <div className="relative mt-6 h-[150px] w-full">
           <Image src="/banner-01.png" fill className="object-cover rounded-xl" alt="banner" />
         </div>
 
-        <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">Agendamentos</h2>
-
-        <Card>
-          <CardContent className="flex justify-between p-0">
-
-            <div className="flex flex-col gap-2 py-5 pl-5 ">
-              <Badge className="w-fit">Confirmado</Badge>
-              <h3 className="font-semibold">Corte de cabelo</h3>
-              <div className="flex items-center gap-2">
-                <Avatar className="h-7 w-7">
-                  <AvatarImage src="https://utfs.io/f/c97a2dc9-cf62-468b-a851-bfd2bdde775f-16p.png" />
-                </Avatar>
-                <p className="text-sm">Barbearia FSW</p>
-              </div>
-            </div>
-
-            <div className="flex flex-col items-center justify-center px-5 border-l-2 border-solid">
-              <p className="text-sm">Fevereiro</p>
-              <p className="text-2xl">01</p>
-              <p className="text-sm">20:00</p>
-            </div>
-
-          </CardContent>
-        </Card>
+        <BookingItem />
 
         <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">Recomendados</h2>
         <div className="flex gap-4 overflow-auto [&::-webkit-scrollbar]:hidden">
