af63366aef94e8eddd9a1f0e340c041da3167e3a
Valmir Vasconcelos de Araujo <valmir.vasconcelos@parauapebas.pa.gov.br>
2025-02-04 14:07:11 -0300

    chore: add next auth setup

--- app/_components/sidebar-sheet.tsx
+++ app/_components/sidebar-sheet.tsx
@@ -25,7 +25,7 @@ const SidebarSheet = () => {
                             <LogInIcon />
                         </Button>
                     </DialogTrigger>
-                    <DialogContent>
+                    <DialogContent className="w-[90%]">
                         <DialogHeader>
                             <DialogTitle>Faça login na plataforma</DialogTitle>
                             <DialogDescription>
